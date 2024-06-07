932ba4ca6274ffd6bae6afc29402c27da2878f82
nataneltau <natanel@mail.tau.ac.il>
2024-06-07 10:03:27 +0300

    Add database, need to work on it and upgrade it (json support, file change, and connect to frontend)

---
+++ try.py
@@ -0,0 +1,29 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://ProjectMainAdmin:SzReRV0ZxjeWm7vN@datastorage1.dlfth1l.mongodb.net/?retryWrites=true&w=majority&appName=DataStorage1"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Access a database
+db = client['Users']
+
+# Access a collection
+collection = db['Influencers']
+
+# Send a ping to confirm a successful connection
+try:
+    document = {"name": "Alice", "age": 25}
+    collection.insert_one(document)
+
+    # Insert multiple documents
+    documents = [
+        {"name": "Bob", "age": 30},
+        {"name": "Charlie", "age": 35}
+    ]
+    collection.insert_many(documents)
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
