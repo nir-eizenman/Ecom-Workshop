8806910d39f81f29eef96fa3d22a701195e4b25b
Maya Marom <116757556+maromaya@users.noreply.github.com>
2024-07-02 20:46:09 +0300

    Update EntityName.py

--- EntityName.py
+++ EntityName.py
@@ -3,6 +3,18 @@ CONST_COMPANY_NAME = 'company_name'
 CONST_EMAIL = 'email'
 CONST_PASSWORD = 'password'
 
+#For Upload Campaign 
+CONST_CAMPAIGN_NAME = 'campaign_name'
+CONST_MAX_PAYMENT = 'max_payment'
+CONST_CAMPAIGN_CATEGORY = 'campaign_category'
+CONST_PRODUCT_IMAGE = 'product_image'
+
+#For Apply for Campaign 
+CONST_NUMBER_STORIES = 'number_of_stories'
+CONST_NUMBER_POSTS = 'number_of_posts'
+CONST_NUMBER_REELS = 'number_of_reels'
+CONST_MIN_PRICE = 'minimum_asking_price'
+
 
 
 
