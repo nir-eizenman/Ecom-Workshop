e15880757ba09c86ffd501083a6f4b83d07d5064
Maya Marom <116757556+maromaya@users.noreply.github.com>
2024-07-15 22:10:48 +0300

    Update api for campaign .py

--- api for campaign .py
+++ api for campaign .py
@@ -1,100 +1,102 @@
-# imports will be the same for everyone so i didnt write them 
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
+from flask_session import Session
+from flask_cors import CORS
+from DatabaseLogic import DatabaseLogic
+import EntityName
 
-# MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')
-db = client['your_database']
-campaigns_collection = db['campaigns']
-applications_collection = db['applications']
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+CORS(app)
+app.secret_key = 'your_secret_key'  # Necessary for session management
+
+
+db = DatabaseLogic("mongodb+srv://ProjectMainAdmin:SzReRV0ZxjeWm7vN@datastorage1.dlfth1l.mongodb.net/?retryWrites=true&w=majority&appName=DataStorage1")
+
 
 @app.route('/upload_campaign/<company_id>', methods=['POST'])
 def upload_campaign(company_id):
-    # Get form data
-    campaign_name = request.form.get('campaign_name')
-    budget = request.form.get('budget')
-    campaign_category = request.form.get('campaign_category')
-    is_active = request.form.get('is_active')
-    about = request.form.get('about')
-    target_location = request.form.get('target_location')
-    target_gender_male = request.form.get('target_gender_male')
-    target_gender_female = request.form.get('target_gender_female')
-    target_gender_other = request.form.get('target_gender_other')
-    target_age_13_17 = request.form.get('target_age_13_17')
-    target_age_18_24 = request.form.get('target_age_18_24')
-    target_age_25_34 = request.form.get('target_age_25_34')
-    target_age_35_44 = request.form.get('target_age_35_44')
-    target_age_45_54 = request.form.get('target_age_45_54')
-    target_age_55_64 = request.form.get('target_age_55_64')
-    target_age_65_plus = request.form.get('target_age_65_plus')
-    
+    required_fields = [
+        EntityName.CONST_CAMPAIGN_NAME, EntityName.CONST_BUDGET, EntityName.CONST_CAMPAIGN_CATEGORY,
+        EntityName.CONST_IS_ACTIVE, EntityName.CONST_ABOUT, EntityName.CONST_TARGET_LOCATION,
+        EntityName.CONST_TARGET_GENDER_MALE, EntityName.CONST_TARGET_GENDER_FEMALE, EntityName.CONST_TARGET_GENDER_OTHER,
+        EntityName.CONST_TARGET_AGE_13_17, EntityName.CONST_TARGET_AGE_18_24, EntityName.CONST_TARGET_AGE_25_34,
+        EntityName.CONST_TARGET_AGE_35_44, EntityName.CONST_TARGET_AGE_45_54, EntityName.CONST_TARGET_AGE_55_64,
+        EntityName.CONST_TARGET_AGE_65_PLUS
+    ]
+
+    data = {field: request.json.get(field) for field in required_fields}
+
     # Check if mandatory fields are provided
-    if not campaign_name or not budget or not campaign_category:
+    if not data[EntityName.CONST_CAMPAIGN_NAME] or not data[EntityName.CONST_BUDGET] or not data[EntityName.CONST_CAMPAIGN_CATEGORY]:
         return jsonify({'error': 'Missing required campaign information'}), 400
-    
+
     # Create campaign data
     campaign_data = {
-        'campaign_name': campaign_name,
-        'budget': int(budget),
-        'campaign_category': {
-            'category': campaign_category == 'true'
+        EntityName.CONST_CAMPAIGN_NAME: data[EntityName.CONST_CAMPAIGN_NAME],
+        EntityName.CONST_BUDGET: int(data[EntityName.CONST_BUDGET]),
+        EntityName.CONST_CAMPAIGN_CATEGORY: {
+            'category': data[EntityName.CONST_CAMPAIGN_CATEGORY] == 'true'
         },
-        'is_active': is_active == 'true',
-        'about': about,
+        EntityName.CONST_IS_ACTIVE: data[EntityName.CONST_IS_ACTIVE] == 'true',
+        EntityName.CONST_ABOUT: data[EntityName.CONST_ABOUT],
         'target_audience': {
             'location': {
-                'country': int(target_location) if target_location else None
+                'country': int(data[EntityName.CONST_TARGET_LOCATION]) if data[EntityName.CONST_TARGET_LOCATION] else None
             },
             'gender': {
-                'male': int(target_gender_male) if target_gender_male else 0,
-                'female': int(target_gender_female) if target_gender_female else 0,
-                'other': int(target_gender_other) if target_gender_other else 0
+                'male': int(data[EntityName.CONST_TARGET_GENDER_MALE]) if data[EntityName.CONST_TARGET_GENDER_MALE] else 0,
+                'female': int(data[EntityName.CONST_TARGET_GENDER_FEMALE]) if data[EntityName.CONST_TARGET_GENDER_FEMALE] else 0,
+                'other': int(data[EntityName.CONST_TARGET_GENDER_OTHER]) if data[EntityName.CONST_TARGET_GENDER_OTHER] else 0
             },
             'age': {
-                '13-17': int(target_age_13_17) if target_age_13_17 else 0,
-                '18-24': int(target_age_18_24) if target_age_18_24 else 0,
-                '25-34': int(target_age_25_34) if target_age_25_34 else 0,
-                '35-44': int(target_age_35_44) if target_age_35_44 else 0,
-                '45-54': int(target_age_45_54) if target_age_45_54 else 0,
-                '55-64': int(target_age_55_64) if target_age_55_64 else 0,
-                '65+': int(target_age_65_plus) if target_age_65_plus else 0
+                '13-17': int(data[EntityName.CONST_TARGET_AGE_13_17]) if data[EntityName.CONST_TARGET_AGE_13_17] else 0,
+                '18-24': int(data[EntityName.CONST_TARGET_AGE_18_24]) if data[EntityName.CONST_TARGET_AGE_18_24] else 0,
+                '25-34': int(data[EntityName.CONST_TARGET_AGE_25_34]) if data[EntityName.CONST_TARGET_AGE_25_34] else 0,
+                '35-44': int(data[EntityName.CONST_TARGET_AGE_35_44]) if data[EntityName.CONST_TARGET_AGE_35_44] else 0,
+                '45-54': int(data[EntityName.CONST_TARGET_AGE_45_54]) if data[EntityName.CONST_TARGET_AGE_45_54] else 0,
+                '55-64': int(data[EntityName.CONST_TARGET_AGE_55_64]) if data[EntityName.CONST_TARGET_AGE_55_64] else 0,
+                '65+': int(data[EntityName.CONST_TARGET_AGE_65_PLUS]) if data[EntityName.CONST_TARGET_AGE_65_PLUS] else 0
             }
         },
         'companyId': ObjectId(company_id)
     }
-    
+
     # Save campaign data to MongoDB
     result = campaigns_collection.insert_one(campaign_data)
-    
+
     # Return the inserted document ID
     return jsonify({'id': str(result.inserted_id), 'campaign_data': campaign_data}), 201
 
 
 @app.route('/apply_for_campaign/<campaign_id>/<influencer_id>', methods=['POST'])
 def apply_for_campaign(campaign_id, influencer_id):
-    # Get form data
-    number_of_stories = request.form.get('number_of_stories')
-    number_of_reels = request.form.get('number_of_reels')
-    number_of_posts = request.form.get('number_of_posts')
-    asking_price = request.form.get('asking_price')
+    required_fields = [
+        EntityName.CONST_NUMBER_STORIES, EntityName.CONST_NUMBER_REELS, EntityName.CONST_NUMBER_POSTS, EntityName.CONST_ASKING_PRICE
+    ]
+
+    data = {field: request.json.get(field) for field in required_fields}
 
     # Check if mandatory fields are provided
-    if not number_of_stories or not number_of_reels or not number_of_posts or not asking_price:
+    if not data[EntityName.CONST_NUMBER_STORIES] or not data[EntityName.CONST_NUMBER_REELS] or not data[EntityName.CONST_NUMBER_POSTS] or not data[EntityName.CONST_ASKING_PRICE]:
         return jsonify({'error': 'Missing required application information'}), 400
-    
+
     # Create application data
     application_data = {
         'campaignId': ObjectId(campaign_id),
         'influencerId': ObjectId(influencer_id),
         'offer': {
-            'stories': int(number_of_stories),
-            'reels': int(number_of_reels),
-            'posts': int(number_of_posts)
+            'stories': int(data[EntityName.CONST_NUMBER_STORIES]),
+            'reels': int(data[EntityName.CONST_NUMBER_REELS]),
+            'posts': int(data[EntityName.CONST_NUMBER_POSTS])
         },
-        'asking_price': int(asking_price)
+        EntityName.CONST_ASKING_PRICE: int(data[EntityName.CONST_ASKING_PRICE])
     }
-    
+
     # Save application data to MongoDB
     result = applications_collection.insert_one(application_data)
-    
+
     # Return the inserted document ID
     return jsonify({'id': str(result.inserted_id), 'application_data': application_data}), 201
 
