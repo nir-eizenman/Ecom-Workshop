7e4e2a7a4b1245833663249644ffa795487798a2
nataneltau <natanel@mail.tau.ac.il>
2024-08-03 22:42:14 +0300

    deploy!

--- frontend/ad-venture-front/src/Company/CompanyHome.jsx
+++ frontend/ad-venture-front/src/Company/CompanyHome.jsx
@@ -64,7 +64,7 @@ const CompanyHome = () => {
 
   useEffect(() => {
     const fetchCampaigns = async () => {
-      const response = await fetch(`http://127.0.0.1:5001/api/company/${sessionStorage.getItem(USER_ID)}/home`);
+      const response = await fetch(`https://api-ad-venture.onrender.com:443/api/company/${sessionStorage.getItem(USER_ID)}/home`);
       const data = await response.json();
       setCampaigns(data);
     };
@@ -82,7 +82,7 @@ const CompanyHome = () => {
 
   const handleAddCampaign = async () => {
     try {
-      const response = await fetch(`http://127.0.0.1:5001/api/company/${sessionStorage.getItem(USER_ID)}/home/create`, {
+      const response = await fetch(`https://api-ad-venture.onrender.com:443/api/company/${sessionStorage.getItem(USER_ID)}/home/create`, {
         method: 'POST',
         body: JSON.stringify({
           ...newCampaign,
@@ -96,7 +96,7 @@ const CompanyHome = () => {
       });
 
       if (response.ok) {
-        const campaignsResponse = await fetch(`http://127.0.0.1:5001/api/company/${sessionStorage.getItem(USER_ID)}/home`);
+        const campaignsResponse = await fetch(`https://api-ad-venture.onrender.com:443/api/company/${sessionStorage.getItem(USER_ID)}/home`);
         const campaignsData = await campaignsResponse.json();
         setCampaigns(campaignsData);
 
@@ -128,7 +128,7 @@ const CompanyHome = () => {
   };
 
   const handleEndCampaign = async (campaignId) => {
-    const response = await fetch(`http://127.0.0.1:5001/api/company/home/${campaignId}/end`, {
+    const response = await fetch(`https://api-ad-venture.onrender.com:443/api/company/home/${campaignId}/end`, {
       method: 'POST',
     });
 
@@ -136,7 +136,7 @@ const CompanyHome = () => {
       setCampaigns(campaigns.filter(campaign => campaign.id !== campaignId));
       console.log(`Successfully ended campaign with ID ${campaignId}`);
 
-      const resultResponse = await fetch(`http://127.0.0.1:5001/api/company/home/${campaignId}/results`);
+      const resultResponse = await fetch(`https://api-ad-venture.onrender.com:443/api/company/home/${campaignId}/results`);
       const resultData = await resultResponse.json();
 
       setResults(resultData.results);
@@ -157,7 +157,7 @@ const CompanyHome = () => {
     const { result_number } = result;
     console.log('results campaign id ' + resultsCampaignId);
     try {
-      const response = await fetch(`http://127.0.0.1:5001/api/company/home/${resultsCampaignId}/results/choose`, {
+      const response = await fetch(`https://api-ad-venture.onrender.com:443/api/company/home/${resultsCampaignId}/results/choose`, {
         method: 'POST',
         body: JSON.stringify({ result_number }),
         headers: {
--- frontend/ad-venture-front/src/SignUp/SignUpInfluencer.jsx
+++ frontend/ad-venture-front/src/SignUp/SignUpInfluencer.jsx
@@ -120,7 +120,7 @@ const SignUpInfluencerForm = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:5001/api/signup/influencer', {
+      const response = await fetch('https://api-ad-venture.onrender.com:443/api/signup/influencer', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
